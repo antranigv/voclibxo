c80f780256846960cf578f497bb3e24990ffe4a7
Antranig Vartanian <antranigv@freebsd.am>
2023-04-05 16:43:45 +0400

    pretty much ready ;)

--- xof_text.c
+++ xof_text.c
@@ -6,6 +6,9 @@ main(int argc, char **argv){
   xo_warn("what am I doing here?...");
   xo_parse_args(argc, argv);
   xo_set_flags(NULL, XOF_COLOR);
-  printf("%lu", sizeof(XOF_COLOR));
+  printf("%lu\n", sizeof(XOF_COLOR));
+  printf("%llu\n", XOF_COLOR);
+  printf("%llu\n", XOF_CONTINUATION);
   xo_emit("what the {C:fg-red}{:what}{C:}\n", "hellll");
+  xo_emit("this is a number : {:number/%d}\n", 4);
 };
